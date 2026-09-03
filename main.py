0683b41ea15dd39fad767a095d1b6c415aad988a
tmallika964-sudo <tmallika964@gmail.com>
2026-09-04 01:23:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Load model using Keras 3
-model = keras.saving.load_model('cotton_disease_model.keras')
+model = keras.models.load_model('cotton_disease_model.h5')
 class_names = [
     'Bacterial Blight',
     'Curl Virus',
